ba75ddf8ce1d4f6537fd8627991d8802301ba144
Konkan <konkan@example.com>
2026-03-26 16:07:04 -0700

    prisma ts setupt

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,16 @@
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
+
 import { PrismaClient } from '../../generated/prisma'
 
-const connectionString = `<%= process.env.DATABASE_URL %>`
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
 
-export { prisma }
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
