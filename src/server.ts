f51bc0efe69161d71499a9d991aebc51a4d572b5
Konkan <konkan@example.com>
2026-04-25 07:40:21 -0700

    fix: vercel serverless compatible

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,28 @@
+// import app from "./app.js";
+// import config from "./config/index.js"
+
+// async function main() {
+//   try {
+//     app.listen(config.port, () => {
+//       console.log(`Example app listening on port ${config.port}`);
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// main();
+
+
 import app from "./app.js";
-import config from "./config/index.js"
+import config from "./config/index.js";
 
-async function main() {
-  try {
-    app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
-    });
-  } catch (err) {
-    console.log(err);
-  }
+
+export default app;
+
+// Local development এর জন্য
+if (process.env.NODE_ENV !== "production") {
+  app.listen(config.port, () => {
+    console.log(`Server running on port ${config.port}`);
+  });
 }
-
-main();
-
-
